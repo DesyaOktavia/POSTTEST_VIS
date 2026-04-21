d4b043e07d3c2688e73430412918edba06c063fc
ECA <desyaoktador@gmail.com>
2026-04-21 23:37:24 +0800

    Posttest 6 Eca

---
+++ POSTTEST_6/FormHome.vb
@@ -0,0 +1,93 @@
+﻿Public Class FormHome
+    ' Harga per gelas (Konstanta)
+    Dim hargaJamu As Integer = 10000
+
+    Private Sub FormHome_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Setup Kolom DataGridView jika belum ada di designer
+        If DGVjamu.ColumnCount = 0 Then
+            DGVjamu.Columns.Add("id", "ID")
+            DGVjamu.Columns.Add("nama", "Nama Pembeli")
+            DGVjamu.Columns.Add("jamu", "Jenis Jamu")
+            DGVjamu.Columns.Add("qty", "Jumlah")
+            DGVjamu.Columns.Add("total", "Total Harga")
+        End If
+    End Sub
+
+    ' Hitung Total Otomatis saat jumlah berubah
+    Private Sub NUMjumlah_ValueChanged(sender As Object, e As EventArgs) Handles NUMjumlah.ValueChanged
+        Dim total As Integer = NUMjumlah.Value * hargaJamu
+        TBtotal.Text = "Rp. " & total.ToString("N0")
+    End Sub
+
+    ' CREATE (SIMPAN)
+    Private Sub Bsimpan_Click(sender As Object, e As EventArgs) Handles Bsimpan.Click
+        If TBnamapembeli.Text = "" Or CBjamu.SelectedIndex = -1 Then
+            MessageBox.Show("Lengkapi data terlebih dahulu!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            Dim id As String = "STK-" & Now.ToString("ssmm")
+            DGVjamu.Rows.Add(id, TBnamapembeli.Text, CBjamu.Text, NUMjumlah.Value, TBtotal.Text)
+            MessageBox.Show("Data berhasil disimpan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Bclear.PerformClick()
+        End If
+    End Sub
+
+    ' UPDATE (UBAH)
+    Private Sub Bupdate_Click(sender As Object, e As EventArgs) Handles Bupdate.Click
+        If DGVjamu.SelectedRows.Count > 0 Then
+            Dim row As DataGridViewRow = DGVjamu.SelectedRows(0)
+            row.Cells(1).Value = TBnamapembeli.Text
+            row.Cells(2).Value = CBjamu.Text
+            row.Cells(3).Value = NUMjumlah.Value
+            row.Cells(4).Value = TBtotal.Text
+            MessageBox.Show("Data berhasil diperbarui!", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    ' DELETE (HAPUS)
+    Private Sub Bhapus_Click(sender As Object, e As EventArgs) Handles Bhapus.Click
+        If DGVjamu.SelectedRows.Count > 0 Then
+            Dim tanya As DialogResult = MessageBox.Show("Hapus data terpilih?", "Hapus", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+            If tanya = DialogResult.Yes Then
+                DGVjamu.Rows.Remove(DGVjamu.SelectedRows(0))
+            End If
+        End If
+    End Sub
+
+    ' SEARCH (CARI)
+    Private Sub Bseacrh_Click(sender As Object, e As EventArgs) Handles Bseacrh.Click
+        Dim keyword As String = TBseacrh.Text.ToLower()
+        For Each row As DataGridViewRow In DGVjamu.Rows
+            If row.Cells(1).Value.ToString().ToLower().Contains(keyword) Then
+                row.Selected = True
+                DGVjamu.FirstDisplayedScrollingRowIndex = row.Index
+                Exit Sub
+            End If
+        Next
+    End Sub
+
+    ' CLEAR
+    Private Sub Bclear_Click(sender As Object, e As EventArgs) Handles Bclear.Click
+        TBnamapembeli.Clear()
+        CBjamu.SelectedIndex = -1
+        NUMjumlah.Value = 0
+        TBtotal.Text = "Rp. 0"
+    End Sub
+
+    ' PINDAH KE FORM CETAK (POIN PLUS MULTI-FORM)
+    Private Sub Bcetak_Click(sender As Object, e As EventArgs) Handles Bcetak.Click
+        If DGVjamu.SelectedRows.Count > 0 Then
+            Dim row As DataGridViewRow = DGVjamu.SelectedRows(0)
+
+            ' Kirim data ke label di FormCetak
+            FormCetak.Lnostruk.Text = "No: " & row.Cells(0).Value.ToString()
+            FormCetak.Lnama.Text = "Nama: " & row.Cells(1).Value.ToString()
+            FormCetak.Ljamu.Text = row.Cells(2).Value.ToString()
+            FormCetak.Ljumlah.Text = "Jumlah: " & row.Cells(3).Value.ToString() & " Gelas"
+            FormCetak.Lharga.Text = row.Cells(4).Value.ToString()
+
+            FormCetak.ShowDialog()
+        Else
+            MessageBox.Show("Pilih data di tabel yang ingin dicetak!", "Pesan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+End Class
---
+++ POSTTEST_6/FormLogin.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Plogin = New Panel()
+        Bkeluar = New Button()
+        Blogin = New Button()
+        TBpassword = New TextBox()
+        TBusername = New TextBox()
+        Lpassword = New Label()
+        Lusername = New Label()
+        Lkedai = New Label()
+        Ljamu = New Label()
+        EPlogin = New ErrorProvider(components)
+        PBlogin = New PictureBox()
+        Plogin.SuspendLayout()
+        CType(EPlogin, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PBlogin, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Plogin
+        ' 
+        Plogin.BackColor = Color.YellowGreen
+        Plogin.Controls.Add(Bkeluar)
+        Plogin.Controls.Add(Blogin)
+        Plogin.Controls.Add(TBpassword)
+        Plogin.Controls.Add(TBusername)
+        Plogin.Controls.Add(Lpassword)
+        Plogin.Controls.Add(Lusername)
+        Plogin.Location = New Point(0, 228)
+        Plogin.Name = "Plogin"
+        Plogin.Size = New Size(565, 249)
+        Plogin.TabIndex = 0
+        ' 
+        ' Bkeluar
+        ' 
+        Bkeluar.BackColor = Color.Salmon
+        Bkeluar.Location = New Point(316, 173)
+        Bkeluar.Name = "Bkeluar"
+        Bkeluar.Size = New Size(119, 45)
+        Bkeluar.TabIndex = 8
+        Bkeluar.Text = "Keluar"
+        Bkeluar.UseVisualStyleBackColor = False
+        ' 
+        ' Blogin
+        ' 
+        Blogin.BackColor = Color.MediumSpringGreen
+        Blogin.Location = New Point(117, 174)
+        Blogin.Name = "Blogin"
+        Blogin.Size = New Size(119, 45)
+        Blogin.TabIndex = 7
+        Blogin.Text = "Login"
+        Blogin.UseVisualStyleBackColor = False
+        ' 
+        ' TBpassword
+        ' 
+        TBpassword.Location = New Point(211, 89)
+        TBpassword.Name = "TBpassword"
+        TBpassword.Size = New Size(258, 27)
+        TBpassword.TabIndex = 6
+        ' 
+        ' TBusername
+        ' 
+        TBusername.Location = New Point(211, 16)
+        TBusername.Name = "TBusername"
+        TBusername.Size = New Size(258, 27)
+        TBusername.TabIndex = 5
+        ' 
+        ' Lpassword
+        ' 
+        Lpassword.AutoSize = True
+        Lpassword.Font = New Font("Segoe UI", 12.0F)
+        Lpassword.Location = New Point(38, 78)
+        Lpassword.Name = "Lpassword"
+        Lpassword.Size = New Size(93, 28)
+        Lpassword.TabIndex = 4
+        Lpassword.Text = "Password"
+        ' 
+        ' Lusername
+        ' 
+        Lusername.AutoSize = True
+        Lusername.Font = New Font("Segoe UI", 12.0F)
+        Lusername.Location = New Point(39, 12)
+        Lusername.Name = "Lusername"
+        Lusername.Size = New Size(99, 28)
+        Lusername.TabIndex = 3
+        Lusername.Text = "Username"
+        ' 
+        ' Lkedai
+        ' 
+        Lkedai.AutoSize = True
+        Lkedai.Font = New Font("Segoe UI", 24.0F)
+        Lkedai.Location = New Point(239, 58)
+        Lkedai.Name = "Lkedai"
+        Lkedai.Size = New Size(230, 54)
+        Lkedai.TabIndex = 1
+        Lkedai.Text = "Kedai Sehat"
+        ' 
+        ' Ljamu
+        ' 
+        Ljamu.AutoSize = True
+        Ljamu.Font = New Font("Segoe UI", 20.0F)
+        Ljamu.Location = New Point(355, 120)
+        Ljamu.Name = "Ljamu"
+        Ljamu.Size = New Size(152, 46)
+        Ljamu.TabIndex = 2
+        Ljamu.Text = "Jamoean"
+        ' 
+        ' EPlogin
+        ' 
+        EPlogin.ContainerControl = Me
+        ' 
+        ' PBlogin
+        ' 
+        PBlogin.Image = My.Resources.Resources.jamoean
+        PBlogin.Location = New Point(39, 23)
+        PBlogin.Name = "PBlogin"
+        PBlogin.Size = New Size(171, 181)
+        PBlogin.SizeMode = PictureBoxSizeMode.StretchImage
+        PBlogin.TabIndex = 3
+        PBlogin.TabStop = False
+        ' 
+        ' FormLogin
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Peru
+        ClientSize = New Size(560, 458)
+        Controls.Add(PBlogin)
+        Controls.Add(Ljamu)
+        Controls.Add(Lkedai)
+        Controls.Add(Plogin)
+        ForeColor = SystemColors.ActiveCaptionText
+        Name = "FormLogin"
+        Text = "KedaiSehat - Menu"
+        Plogin.ResumeLayout(False)
+        Plogin.PerformLayout()
+        CType(EPlogin, ComponentModel.ISupportInitialize).EndInit()
+        CType(PBlogin, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Plogin As Panel
+    Friend WithEvents Lpassword As Label
+    Friend WithEvents Lusername As Label
+    Friend WithEvents Lkedai As Label
+    Friend WithEvents Ljamu As Label
+    Friend WithEvents TBpassword As TextBox
+    Friend WithEvents TBusername As TextBox
+    Friend WithEvents Bkeluar As Button
+    Friend WithEvents Blogin As Button
+    Friend WithEvents EPlogin As ErrorProvider
+    Friend WithEvents PBlogin As PictureBox
+
+End Class
